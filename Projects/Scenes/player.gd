be3a1c197b733048b8cd4628d0a634e8c2e43484
FattestCatto <daniel.mosneagu@cearvs.ro>
2024-07-13 22:34:17 +0300

    Updated the code to use a class instead of variables

--- Projects/Scenes/player.gd
+++ Projects/Scenes/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
+class speed_class:
+	var x = 0
+	var y = 0
+	@export var loss = 5
+	@export var gain = 5
+	@export var max = 100
 
-var x_speed = 0
-var y_speed = 0
-@export var speed_loss = 2
-@export var speed_gain = 5
-@export var max_speed = 100
-
+var speed = speed_class.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(640,360) # Place in the middle of the screen.
@@ -13,40 +14,40 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	var direction = Input.get_vector("left","right","up","down")
-	velocity_calc(direction)
-	print(y_speed," ", direction.y)
-	velocity = Vector2(x_speed,y_speed)
+	velocity_calc(direction,speed)
+	print(speed.y," ", direction.y)
+	velocity = Vector2(speed.x,speed.y)
 	move_and_slide()
 
-func velocity_calc(direction):
+func velocity_calc(direction, speed):
 											## X axis speed and velocity management
 	if direction.x != 0: #If moving
-		x_speed += direction.x * speed_gain #Gain speed on the respective X axis direction (if negative direction = left)
+		speed.x += direction.x * speed.gain #Gain speed on the respective X axis direction (if negative direction = left)
 	else: #if left or right are not pressed
-		if x_speed < 0: #if the speed is going to the left
-			if x_speed <= -speed_loss: 
-				x_speed += speed_loss #Start losing speed
+		if speed.x < 0: #if the speed is going to the left
+			if speed.x <= -speed.loss: 
+				speed.x += speed.loss #Start losing speed
 			else:
-				x_speed = 0
-		elif x_speed > 0:#if the speed is going to the right
-			if x_speed >= speed_loss:
-				x_speed -= speed_loss #Start losing speed
+				speed.x = 0
+		elif speed.x > 0:#if the speed is going to the right
+			if speed.x >= speed.loss:
+				speed.x -= speed.loss #Start losing speed
 			else:
-				x_speed = 0				## End of X axis management
+				speed.x = 0				## End of X axis management
 
 #Similar or same actions on y axis
 
 	if direction.y != 0:					## Y axis speed and velocity management
-		y_speed += direction.y * speed_gain #Gain speed on the respective y axis direction (if negative direction = left)
+		speed.y += direction.y * speed.gain #Gain speed on the respective y axis direction (if negative direction = left)
 	else: #if left or right are not pressed
-		if y_speed < 0: #if the speed is going to the left
-			if y_speed <= -speed_loss: 
-				y_speed += speed_loss #Start losing speed
+		if speed.y < 0: #if the speed is going to the left
+			if speed.y <= -speed.loss: 
+				speed.y += speed.loss #Start losing speed
 			else:
-				y_speed = 0
-		elif y_speed > 0:#if the speed is going to the right
-			if y_speed >= speed_loss:
-				y_speed -= speed_loss #Start losing speed
+				speed.y = 0
+		elif speed.y > 0:#if the speed is going to the right
+			if speed.y >= speed.loss:
+				speed.y -= speed.loss #Start losing speed
 			else:
-				y_speed = 0				## End of y axis management
-			y_speed -= speed_loss 			## End of Y axis management
+				speed.y = 0				## End of y axis management
+			speed.y -= speed.loss 			## End of Y axis management
