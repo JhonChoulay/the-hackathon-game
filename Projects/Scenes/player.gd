4c69b8853f4b98afa9e79bfbaf62420f0d345e85
FattestCatto <daniel.mosneagu@cearvs.ro>
2024-07-15 17:33:14 +0300

    Made movement respect the exported variable "maximum" from the class "speed" Made the player dynamically spawn at the centre of the screen no matter the resolution of the game

--- Projects/Scenes/player.gd
+++ Projects/Scenes/player.gd
@@ -4,25 +4,25 @@ class speed_class:
 	var y = 0
 	@export var loss = 5
 	@export var gain = 5
-	@export var maximum = 100
+	@export var maximum = 400
 
 var speed = speed_class.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(640,360) # Place in the middle of the screen.
+	var resolution = get_viewport().get_visible_rect()
+	position = Vector2(resolution.size[0]/2, resolution.size[1]/2)	# Place the player in the middle of the screen.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	var direction = Input.get_vector("left","right","up","down")
-	velocity_calc(direction,speed)
-	print(speed.y," ", direction.y)
+	velocity_calc(direction)
 	velocity = Vector2(speed.x,speed.y)
 	move_and_slide()
 
-func velocity_calc(direction, speed):
+func velocity_calc(direction_var):
 											## X axis speed and velocity management
-	if direction.x != 0: #If moving
-		speed.x += direction.x * speed.gain #Gain speed on the respective X axis direction (if negative direction = left)
+	if direction_var.x != 0: #If moving
+		speed.x += direction_var.x * speed.gain #Gain speed on the respective X axis direction_var (if negative direction_var = left)
 	else: #if left or right are not pressed
 		if speed.x < 0: #if the speed is going to the left
 			if speed.x <= -speed.loss: 
@@ -34,11 +34,13 @@ func velocity_calc(direction, speed):
 				speed.x -= speed.loss #Start losing speed
 			else:
 				speed.x = 0				## End of X axis management
+	if abs(speed.x) > speed.maximum:
+		speed.x = direction_var.x * speed.maximum
 
 #Similar or same actions on y axis
 
-	if direction.y != 0:					## Y axis speed and velocity management
-		speed.y += direction.y * speed.gain #Gain speed on the respective y axis direction (if negative direction = left)
+	if direction_var.y != 0:					## Y axis speed and velocity management
+		speed.y += direction_var.y * speed.gain #Gain speed on the respective y axis direction_var (if negative direction_var = left)
 	else: #if left or right are not pressed
 		if speed.y < 0: #if the speed is going to the left
 			if speed.y <= -speed.loss: 
@@ -50,3 +52,5 @@ func velocity_calc(direction, speed):
 				speed.y -= speed.loss #Start losing speed
 			else:
 				speed.y = 0				## End of y axis management
+	if abs(speed.y) > speed.maximum:
+		speed.y = direction_var.y * speed.maximum
