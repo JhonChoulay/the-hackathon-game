4ae4d7a8f2e40cb53e1561ebfae408713f665292
FattestCatto <daniel.mosneagu@cearvs.ro>
2024-07-16 15:02:17 +0300

    Temporary unfinished main menu update

---
+++ Projects/Scenes/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var resolution = get_viewport().get_visible_rect()
+	var sprite : Array = [$Sprite.texture.get_width(),$Sprite.texture.get_height()]
+	scale.x = float(resolution.size[0]) / float(sprite[0])
+	scale.y = float(resolution.size[1]) / float(sprite[1])
+	position = Vector2(resolution.size[0] / 2, resolution.size[1] / 2)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
