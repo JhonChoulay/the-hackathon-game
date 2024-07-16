33e7d33ed793dce99ce8abc5cb7eccb5a87c212a
FattestCatto <daniel.mosneagu@cearvs.ro>
2024-07-16 13:17:05 +0300

    Fixed movement system and added temporary edge collisions

--- Projects/Scenes/player.gd
+++ Projects/Scenes/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 class speed_class:
 	var x = 0
 	var y = 0
-	@export var loss = 5
+	@export var loss = 2
 	@export var gain = 5
 	@export var maximum = 400
 
@@ -23,34 +23,32 @@ func velocity_calc(direction_var):
 											## X axis speed and velocity management
 	if direction_var.x != 0: #If moving
 		speed.x += direction_var.x * speed.gain #Gain speed on the respective X axis direction_var (if negative direction_var = left)
+		speed.x = max(min(speed.x,speed.maximum), -speed.maximum) #inner min clamps the upper limit of speed.x/y, outer max clamps the lower limit
+		speed.y = max(min(speed.y,speed.maximum), -speed.maximum)
 	else: #if left or right are not pressed
 		if speed.x < 0: #if the speed is going to the left
-			if speed.x <= -speed.loss: 
-				speed.x += speed.loss #Start losing speed
-			else:
-				speed.x = 0
+			if is_on_wall():
+				speed.x = 1
+			speed.x = min(speed.x + speed.loss, 0) #Clamp the negative speed to the minimum of the 2 results
 		elif speed.x > 0:#if the speed is going to the right
-			if speed.x >= speed.loss:
-				speed.x -= speed.loss #Start losing speed
-			else:
-				speed.x = 0				## End of X axis management
-	if abs(speed.x) > speed.maximum:
-		speed.x = direction_var.x * speed.maximum
+			if is_on_wall():
+				speed.x = -1
+			speed.x = max(speed.x - speed.loss, 0) #Clamp the positive speed to the maximum of the 2 results
+			## End of X axis management
 
 #Similar or same actions on y axis
 
-	if direction_var.y != 0:					## Y axis speed and velocity management
+	if direction_var.y != 0: #If moving
 		speed.y += direction_var.y * speed.gain #Gain speed on the respective y axis direction_var (if negative direction_var = left)
+		speed.y = max(min(speed.y,speed.maximum), -speed.maximum) #inner min clamps the upper limit of speed.x/y, outer max clamps the lower limit
+		speed.y = max(min(speed.y,speed.maximum), -speed.maximum)
 	else: #if left or right are not pressed
-		if speed.y < 0: #if the speed is going to the left
-			if speed.y <= -speed.loss: 
-				speed.y += speed.loss #Start losing speed
-			else:
-				speed.y = 0
-		elif speed.y > 0:#if the speed is going to the right
-			if speed.y >= speed.loss:
-				speed.y -= speed.loss #Start losing speed
-			else:
-				speed.y = 0				## End of y axis management
-	if abs(speed.y) > speed.maximum:
-		speed.y = direction_var.y * speed.maximum
+		if speed.y < 0: #if the speed is going up
+			if is_on_ceiling():
+				speed.y = 1
+			speed.y = min(speed.y + speed.loss, 0) #Clamp the negative speed to the minimum of the 2 results
+		elif speed.y > 0:#if the speed is going down
+			if is_on_floor():
+				speed.y = -1
+			speed.y = max(speed.y - speed.loss, 0) #Clamp the positive speed to the maximum of the 2 results
+			## End of y axis management
