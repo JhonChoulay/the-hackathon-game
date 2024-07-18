7b2983d3fd6645e54f3ade520bed389bff4a21ee
FattestCatto <mosneagudanielv657@gmail.com>
2024-07-18 17:47:45 +0300

    Created a working scene that saves and sets resolution. Yet to implement fully

--- Projects/Scenes/options_menu.gd
+++ Projects/Scenes/options_menu.gd
@@ -1,16 +1,27 @@
-extends Node2D
+extends HBoxContainer
 
-func _on_resolution_list_item_selected(index):
-	var config = ConfigFile.new()
+var resolutions : Dictionary = {
+	"1920x1080":Vector2(1920,1080),
+	"1600x900":Vector2(1600,900),
+	"1280x720":Vector2(1280,720),
+	"1024x600":Vector2(1024,600),
+	"800x600":Vector2(800,600)}
+
+var config = ConfigFile.new()
+
+func _ready():
+	var index = 0
+	for i in resolutions:
+		get_child(0).add_item(i,index)
+		if Vector2i(resolutions[i]) == get_viewport().content_scale_size:
+			$OptionButton.select(index)
+		index +=1
+
+func _on_option_button_item_selected(index):
+	var res = resolutions.get(get_child(0).get_item_text(index))
+	print(res)
+	get_viewport().content_scale_size = res
 	config.load("res://Config/config.ini")
-	match index:
-		0:
-			config.set_value("Settings","screen_width",1920)
-			config.set_value("Settings","screen_height",1080)
-			config.save("res://Config/config.ini")
-		1:
-			config.set_value("Settings","screen_width",1080)
-			config.set_value("Settings","screen_height",720)
-			config.save("res://Config/config.ini")
-		_:
-			print("err")
+	config.set_value("Settings","screen_width",res.x)
+	config.set_value("Settings","screen_height",res.y)
+	config.save("res://Config/config.ini")
