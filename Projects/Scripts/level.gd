c70e7e39b9e711e641c9c61f126eb0c6fb63ab68
FattestCatto <daniel.mosneagu@cearvs.ro>
2024-07-22 20:07:42 +0300

    Updated the game.zip

--- Projects/Scripts/level.gd
+++ Projects/Scripts/level.gd
@@ -3,10 +3,4 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var window = get_window()
-	if !Resources.windowed:
-		if Resources.borderless:
-			window.borderless = Resources.borderless
-		else:
-			window.exclusive = Resources.exclusive_fullscreen
 	get_viewport().content_scale_size = Resources.resolution
