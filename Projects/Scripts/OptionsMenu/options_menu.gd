66a9cc52d856319ce9d409a34b1edfc2440c80e2
FattestCatto <daniel.mosneagu@cearvs.ro>
2024-07-22 22:17:06 +0300

    Almost finished working on the window options button.

--- Projects/Scripts/OptionsMenu/options_menu.gd
+++ Projects/Scripts/OptionsMenu/options_menu.gd
@@ -10,23 +10,33 @@ var resolutions : Dictionary = {
 
 var config = ConfigFile.new()
 var res
-var ex_res
+var window_mode : int
 @onready var timer = $ConfirmationPanel/Timer
 @onready var res_button = get_node("ResolutionOptions")
+@onready var window = get_window()
 
 func _ready():
+	#Set resolution
 	var index = 0
 	for i in resolutions:
 		res_button.add_item(i,index)
 		if Vector2i(resolutions[i]) == get_viewport().content_scale_size:
 			res_button.select(index)
 		index +=1
-	#Set window mode
+	#
+	##Set window mode
 	index = 0
-	for i in ["Windowed","Borderless","Fullscreen","Exclusive Fullscreen"]:
+	for i in ["Windowed","Fullscreen","Exclusive Fullscreen"]:
 		$WindowOptions.add_item(i,index)
-	res = get_viewport().content_scale_size
-	ex_res = res
+		if index + 2 == Resources.window_mode:
+			$WindowOptions.select(index)
+		index += 1
+	for i in [0,3,4]:
+		if i == Resources.window_mode:
+			window.set_mode(i)
+			window_mode = i
+	##
+	res = get_viewport().content_scale_size 
 	#windowed = 0, minimized = 1, maximized = 2 fullscreen = 3, exclusive = 4
 
 func _on_option_button_item_selected(index):
@@ -34,32 +44,66 @@ func _on_option_button_item_selected(index):
 
 func _on_apply_button_pressed():
 	get_viewport().content_scale_size = res
+	window.set_mode(window_mode)
 	timer.start()
-	config_resolution(res)
-	Resources.resolution = res
 	$ConfirmationPanel.visible = true
 
 func _on_revert_button_pressed():
-	config_resolution(ex_res)
-	Resources.resolution = res
-	get_viewport().content_scale_size = ex_res
+	#Resolution
+	get_viewport().content_scale_size = Resources.resolution
 	res_indexer()
-	res = get_viewport().content_scale_size
+	res = Resources.resolution
+	#
+	##Window
+	window.set_mode(Resources.window_mode)
+	window_indexer()
+	window_mode = Resources.window_mode
+	##
 	$ConfirmationPanel.visible = false
 
 func _on_timer_timeout():
-	config_resolution(ex_res)
-	Resources.resolution = res
-	get_viewport().content_scale_size = ex_res
+	#Resolution
+	get_viewport().content_scale_size = Resources.resolution
 	res_indexer()
-	res = get_viewport().content_scale_size
+	res = Resources.resolution
+	#
+	##Window Mode
+	window.set_mode(Resources.window_mode)
+	window_indexer()
+	window_mode = Resources.window_mode
+	##
 	$ConfirmationPanel.visible = false
 
 func _on_confirm_button_pressed():
+	#Resolution
+	Resources.resolution = res
+	config_resolution(Resources.resolution)
+	#
+	##Window
+	Resources.window_mode = window_mode
+	config_window(window_mode)
+	##
 	$ConfirmationPanel.visible = false
-	ex_res = res
+
+func _on_window_options_item_selected(index):
+	if index != 0 :
+		window_mode = index + 2
+	else:
+		window_mode = index
 
 ### Custom Functions ###
+func config_window(window_type : int):
+	config.load("res://Config/config.ini")
+	config.set_value("Settings","window_mode",window_type)
+	config.save("res://Config/config.ini")
+
+func window_indexer():
+	var index = 0
+	for i in [0,3,4]:
+		if i == Resources.window_mode:
+			$WindowOptions.select(index)
+		index +=1
+
 func config_resolution(resolution_var:Vector2):
 	config.load("res://Config/config.ini")
 	config.set_value("Settings","screen_width",int(resolution_var.x))
@@ -72,3 +116,4 @@ func res_indexer():
 		if Vector2i(resolutions[i]) == get_viewport().content_scale_size:
 			res_button.select(index)
 		index +=1
+
--- Projects/Scripts/OptionsMenu/reset_label.gd
+++ Projects/Scripts/OptionsMenu/reset_label.gd
@@ -1,11 +1,5 @@
 extends Node
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.visible = false
-
-
-
-func _on_timer_timeout():
 	self.visible = false
